d1c7efcd2002c9e3d54da19f3d76cbb129aa01df
Saurabh Prakash <saurabhp75@gmail.com>
2025-06-14 18:05:09 +0530

    Remove uneeded components from layout.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,5 @@ import { Stack } from "expo-router";
 import { View, Text } from "react-native";
 
 export default function RootLayout() {
-  return (
-    <View style={{ flex: 1 }}>
-      <Stack />
-      <Text>Footer</Text>
-    </View>
-  );
+  return <Stack />;
 }
