d5e6d32a7a008becceffc608497359d481b83eef
Saurabh Prakash <saurabhp75@gmail.com>
2025-06-14 17:48:43 +0530

    Add footer in layout.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
-import { Slot } from 'expo-router';
-import React from 'react';
+import { Slot } from "expo-router";
+import React from "react";
+import { View, Text } from "react-native";
 
 export default function RootLayout() {
-  return <Slot />;
+  return (
+    <View style={{ flex: 1 }}>
+      <Slot />
+      <Text>Footer</Text>
+    </View>
+  );
 }
