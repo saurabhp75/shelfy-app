24b3dd0911b1df0ad23e45120d02dab81e1c6839
Saurabh Prakash <saurabhp75@gmail.com>
2025-06-14 18:54:21 +0530

    Set individual and global screen config.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,20 @@
 import { Stack } from "expo-router";
-import { View, Text } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: "#ddd" },
+        headerTintColor: "#333",
+      }}
+    >
+      {/* Individual Screens */}
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="about" options={{ title: "About" }} />
+      <Stack.Screen
+        name="contact"
+        options={{ title: "Contact", headerShown: false }}
+      />
+    </Stack>
+  );
 }
