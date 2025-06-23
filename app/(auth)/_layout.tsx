f2cc248a65874ac3a9d2b39706c1cfdadeee73cf
Saurabh Prakash <saurabhp75@gmail.com>
2025-06-23 06:09:48 +0530

    Protect the routes.

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,11 +1,12 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import GuestOnly from "../../components/auth/GuestOnly";
 
 export default function AuthLayout() {
   return (
-    <>
+    <GuestOnly>
       <StatusBar style="auto" />
       <Stack screenOptions={{ headerShown: false, animation: "none" }} />
-    </>
+    </GuestOnly>
   );
 }
